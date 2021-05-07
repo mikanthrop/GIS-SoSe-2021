784f349685de035f241e7f3de9f7e633eab7d9ff
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-05-07 20:55:07 +0200

    getting started with the exercise

--- Kapitel_2/Kapitelaufgabe_2/script.js
+++ Kapitel_2/Kapitelaufgabe_2/script.js
@@ -1,2 +1,9 @@
 "use strict";
+var Kapitelaufgabe2;
+(function (Kapitelaufgabe2) {
+    let lookingWay;
+    let earShape;
+    let bodyStartTop;
+    let bodyStartBottom;
+})(Kapitelaufgabe2 || (Kapitelaufgabe2 = {}));
 //# sourceMappingURL=script.js.map
---
+++ Kapitel_2/Kapitelaufgabe_2/script.ts
@@ -0,0 +1,15 @@
+namespace Kapitelaufgabe2 {
+
+    interface head {
+        let lookingWay: string;
+        let earShape: string;
+        let bodyStartTop: number;
+        let bodyStartBottom: number;
+    }
+    interface upperbody {
+
+    }
+    interface lowerbody {
+
+    }
+}
