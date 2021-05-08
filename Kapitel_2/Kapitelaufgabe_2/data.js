f0a62f1abd61c2d9ede56d0f85d113741ad4f8ba
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-05-08 19:48:04 +0200

    saving the data

---
+++ Kapitel_2/Kapitelaufgabe_2/data.js
@@ -0,0 +1,83 @@
+"use strict";
+var Kapitelaufgabe2;
+(function (Kapitelaufgabe2) {
+    //heads
+    let head1 = {
+        picture: "../Kapitelaufgabe_2/dagobert-duck.png",
+        name: "Dagobert Duck",
+        animal: "Ente",
+        hat: true,
+        color: "Schwarz"
+    };
+    let head2 = {
+        picture: "../Kapitelaufgabe_2/mickey_mouse.png",
+        name: "Mickey Mouse",
+        animal: "Maus",
+        hat: false
+    };
+    let head3 = {
+        picture: "../Kapitelaufgabe_2/goofy.png",
+        name: "Goofy",
+        animal: "Hund",
+        hat: true,
+        color: "Blau"
+    };
+    let head4 = {
+        picture: "../Kapitelaufgabe_2/klarabella.png",
+        name: "Klarabella Kuh",
+        animal: "Kuh",
+        hat: false
+    };
+    Kapitelaufgabe2.headArray = [head1, head2, head3, head4];
+    //bodies
+    let body1 = {
+        name: "Dagobert Duck",
+        animal: "Ente",
+        clothes: true,
+        color: "Rot"
+    };
+    let body2 = {
+        name: "Mickey Mouse",
+        animal: "Maus",
+        clothes: true,
+        color: "Rot"
+    };
+    let body3 = {
+        name: "Goofy",
+        animal: "Hund",
+        clothes: true,
+        color: "Rot" + ", " + "blau"
+    };
+    let body4 = {
+        name: "Klarabella Kuh",
+        animal: "Kuh",
+        clothes: true,
+        color: "Gelbd"
+    };
+    //feet
+    let feet1 = {
+        name: "Dagobert Duck",
+        animal: "Ente",
+        shoes: true,
+        color: "Blau"
+    };
+    let feet2 = {
+        name: "Mickey Mouse",
+        animal: "Maus",
+        shoes: true,
+        color: "Gelb"
+    };
+    let feet3 = {
+        name: "Goofy",
+        animal: "Hund",
+        shoes: true,
+        color: "Braun"
+    };
+    let feet4 = {
+        name: "Klarabella Kuh",
+        animal: "Kuh",
+        shoes: true,
+        color: "Rot"
+    };
+})(Kapitelaufgabe2 || (Kapitelaufgabe2 = {}));
+//# sourceMappingURL=data.js.map
