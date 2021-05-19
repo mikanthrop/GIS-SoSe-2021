1a7dc5864b3a177bcbb20509bd8b578104db8870
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-05-19 13:49:44 +0200

    changing script.ts and data.js accordingly

---
+++ Kapitel_2/Kapitelaufgabe_2/source/data.ts
@@ -0,0 +1,19 @@
+namespace Kapitelaufgabe2 {
+
+    export interface APart {
+        picture: string;
+        name: string;
+        animal: string;
+    }
+    export interface AllParts {
+        heads: APart[];
+        bodies: APart[];
+        feet: APart[];
+    }
+    export interface PickedBodyParts {
+        head: APart;
+        body: APart;
+        feet: APart;
+    }
+}
+    
