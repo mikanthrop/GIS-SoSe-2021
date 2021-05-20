cd64abc3f869dc17a9ecf0bccbebf36448c803dd
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-05-20 15:08:58 +0200

    finishing everything up

--- Kapitel_2/Kapitelaufgabe_2/source/script.js
+++ Kapitel_2/Kapitelaufgabe_2/source/script.js
@@ -1,22 +1,29 @@
 "use strict";
 var Kapitelaufgabe2;
 (function (Kapitelaufgabe2) {
-    window.addEventListener("load", init);
-    function init() {
-        getData("../source/data.json");
-    }
+    let parsedData;
     async function getData(_url) {
         let response = await fetch(_url);
-        console.log(Kapitelaufgabe2.parsedData);
         let jsonObj = await response.json();
-        console.log(Kapitelaufgabe2.parsedData);
-        Kapitelaufgabe2.parsedData = JSON.parse(JSON.stringify(jsonObj));
-        console.log(Kapitelaufgabe2.parsedData);
+        parsedData = JSON.parse(JSON.stringify(jsonObj));
     }
     let currentPage = getSubpage();
     console.log(currentPage);
     function getSubpage() {
         return window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1);
+    }
+    async function getServerAnswer(_url) {
+        let browserCacheData = JSON.parse(JSON.stringify(sessionStorage));
+        let query = new URLSearchParams(browserCacheData);
+        _url = _url + "?" + query.toString();
+        let response = await fetch(_url);
+        let text = await response.text();
+        let displayText = JSON.parse(text);
+        if (displayText.error != undefined)
+            return displayText.error;
+        if (displayText.message != undefined)
+            return displayText.message;
+        return "Ooops, something went wrong.";
     }
     //creating basic structure header main and footer in the body
     let header = document.createElement("header");
@@ -31,6 +38,10 @@ var Kapitelaufgabe2;
     homeLink.setAttribute("style", "padding: " + 20 + "px; margin: " + 5 + "px; font-size: " + 40 + "px; text-transform: capitalize; font-weight: bold");
     homeLink.setAttribute("href", "../html/index.html");
     header.appendChild(homeLink);
+    // creating div for server message on finish.html
+    let showServerMessage = document.createElement("div");
+    showServerMessage.setAttribute("id", "ServerMessage");
+    main.appendChild(showServerMessage);
     //creating the subtext of the headline for that specific site without anything in it
     let infoLine = document.createElement("p");
     infoLine.setAttribute("style", "fontsize: " + 30 + "px; color: inherit; margin-bottom: " + 20 + "px");
@@ -77,11 +88,11 @@ var Kapitelaufgabe2;
             sessionStorage.setItem(_bodyPart + "Name", _part.name);
             sessionStorage.setItem(_bodyPart + "Animal", _part.animal);
             if (_bodyPart == "head")
-                window.open("../Kapitelaufgabe_2/body.html", "_self");
+                window.open("../html/body.html", "_self");
             if (_bodyPart == "body")
-                window.open("../Kapitelaufgabe_2/feet.html", "_self");
+                window.open("../html/feet.html", "_self");
             if (_bodyPart == "feet")
-                window.open("../Kapitelaufgabe_2/finish.html", "_self");
+                window.open("../html/finish.html", "_self");
         }
     }
     function showChoices() {
@@ -103,61 +114,66 @@ var Kapitelaufgabe2;
             choicesSoFar.appendChild(feetPic);
         }
     }
-    if (currentPage == "index.html") {
-        let info1 = document.createElement("p");
-        info1.innerText = "Als Projekt für mein Studium möchte ich auf dieser Webseite den Benutzer einen Entenhausener aus Bildern ausgewählter Figuren zusammenbauen lassen.";
-        main.appendChild(info1);
-        let info2 = document.createElement("p");
-        info2.innerText = "Bitte drücken Sie auf den untenstehenden Button, um zu beginnen.";
-        main.appendChild(info2);
-        let startButton = document.createElement("button");
-        startButton.innerText = "Los geht's!";
-        startButton.addEventListener("click", handleStartButtonClick);
-        main.appendChild(startButton);
-        function handleStartButtonClick(_event) {
-            window.open("../html/head.html", "_self");
+    async function buildPage() {
+        await getData("../source/data.json");
+        if (currentPage == "index.html") {
+            let info1 = document.createElement("p");
+            info1.innerText = "Als Projekt für mein Studium möchte ich auf dieser Webseite den Benutzer einen Entenhausener aus Bildern ausgewählter Figuren zusammenbauen lassen.";
+            main.appendChild(info1);
+            let info2 = document.createElement("p");
+            info2.innerText = "Bitte drücken Sie auf den untenstehenden Button, um zu beginnen.";
+            main.appendChild(info2);
+            let startButton = document.createElement("button");
+            startButton.innerText = "Los geht's!";
+            startButton.addEventListener("click", handleStartButtonClick);
+            main.appendChild(startButton);
+            function handleStartButtonClick(_event) {
+                window.open("../html/head.html", "_self");
+            }
+        }
+        if (currentPage == "head.html") {
+            for (let i = 0; i < parsedData.heads.length; i++) {
+                infoLine.innerText = "Bitte wählen Sie einen Kopf, um Ihren eigenen Entenhausener zusammenzusetzen.";
+                createAPart(parsedData.heads[i], "head");
+            }
+        }
+        if (currentPage == "body.html") {
+            showChoices();
+            for (let i = 0; i < parsedData.bodies.length; i++) {
+                infoLine.innerText = "Bitte wählen Sie einen Körper, um Ihren eigenen Entenhausener zusammenzusetzen.";
+                createAPart(parsedData.bodies[i], "body");
+            }
+        }
+        if (currentPage == "feet.html") {
+            showChoices();
+            for (let i = 0; i < parsedData.feet.length; i++) {
+                infoLine.innerText = "Bitte wählen Sie die Füße, um Ihren eigenen Entenhausener zusammenzusetzen.";
+                createAPart(parsedData.feet[i], "feet");
+            }
+        }
+        if (currentPage == "finish.html") {
+            showServerMessage.innerText = "Server sagt: " + await getServerAnswer("https://gis-communication.herokuapp.com");
+            infoLine.innerText = "Das ist Ihr Entenhausener:";
+            showChoices();
+            let name = document.createElement("p");
+            let animal = document.createElement("p");
+            name.innerText = "Name: " + sessionStorage.getItem("headName") + sessionStorage.getItem("bodyName") + sessionStorage.getItem("feetName");
+            animal.innerText = "Tier: " + sessionStorage.getItem("headAnimal") + "-" + sessionStorage.getItem("bodyAnimal") + "-" + sessionStorage.getItem("feetAnimal");
+            choicesSoFar.appendChild(name);
+            choicesSoFar.appendChild(animal);
+            let thankYouLine = document.createElement("p");
+            thankYouLine.innerText = "Danke, dass Sie meine Website benutzt haben. Viel Spaß mit Ihrem eigenen Entenhausener.";
+            thankYouLine.setAttribute("style", "margin-bottom: " + 20 + "px");
+            main.appendChild(thankYouLine);
+            let newStart = document.createElement("button");
+            newStart.innerText = "Versuchen wir das nochmal.";
+            newStart.addEventListener("click", handleButtonClick);
+            main.appendChild(newStart);
+            function handleButtonClick(_event) {
+                window.open("../html/head.html", "_self");
+            }
         }
     }
-    if (currentPage == "head.html") {
-        for (let i = 0; i < Kapitelaufgabe2.parsedData.heads.length; i++) {
-            infoLine.innerText = "Bitte wählen Sie einen Kopf, um Ihren eigenen Entenhausener zusammenzusetzen.";
-            createAPart(Kapitelaufgabe2.parsedData.heads[i], "head");
-        }
-    }
-    if (currentPage == "body.html") {
-        showChoices();
-        for (let i = 0; i < Kapitelaufgabe2.parsedData.bodies.length; i++) {
-            infoLine.innerText = "Bitte wählen Sie einen Körper, um Ihren eigenen Entenhausener zusammenzusetzen.";
-            createAPart(Kapitelaufgabe2.parsedData.bodies[i], "body");
-        }
-    }
-    if (currentPage == "feet.html") {
-        showChoices();
-        for (let i = 0; i < Kapitelaufgabe2.parsedData.feet.length; i++) {
-            infoLine.innerText = "Bitte wählen Sie die Füße, um Ihren eigenen Entenhausener zusammenzusetzen.";
-            createAPart(Kapitelaufgabe2.parsedData.feet[i], "feet");
-        }
-    }
-    if (currentPage == "finish.html") {
-        infoLine.innerText = "Das ist Ihr Entenhausener:";
-        showChoices();
-        let name = document.createElement("p");
-        let animal = document.createElement("p");
-        name.innerText = "Name: " + sessionStorage.getItem("headName") + sessionStorage.getItem("bodyName") + sessionStorage.getItem("feetName");
-        animal.innerText = "Tier: " + sessionStorage.getItem("headAnimal") + "-" + sessionStorage.getItem("bodyAnimal") + "-" + sessionStorage.getItem("feetAnimal");
-        choicesSoFar.appendChild(name);
-        choicesSoFar.appendChild(animal);
-        let thankYouLine = document.createElement("p");
-        thankYouLine.innerText = "Danke, dass Sie meine Website benutzt haben. Viel Spaß mit Ihrem eigenen Entenhausener.";
-        thankYouLine.setAttribute("style", "margin-bottom: " + 20 + "px");
-        main.appendChild(thankYouLine);
-        let newStart = document.createElement("button");
-        newStart.innerText = "Versuchen wir das nochmal.";
-        newStart.addEventListener("click", handleButtonClick);
-        main.appendChild(newStart);
-        function handleButtonClick(_event) {
-            window.open("../Kapitelaufgabe_2/head.html", "_self");
-        }
-    }
+    buildPage();
 })(Kapitelaufgabe2 || (Kapitelaufgabe2 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel_2/Kapitelaufgabe_2/source/script.ts
+++ Kapitel_2/Kapitelaufgabe_2/source/script.ts
@@ -1,20 +1,11 @@
 namespace Kapitelaufgabe2 {
 
-    export let parsedData: AllParts;
-
-    window.addEventListener("load", init);
-
-    function init(): void {
-        getData("../source/data.json");
-    }
+    let parsedData: AllParts;
 
     async function getData(_url: RequestInfo): Promise<void> {
         let response: Response = await fetch(_url);
-        console.log(parsedData);
         let jsonObj: string = await response.json();
-        console.log(parsedData);
         parsedData = JSON.parse(JSON.stringify(jsonObj));
-        console.log(parsedData);
     }
 
     let currentPage: string = getSubpage();
@@ -22,6 +13,23 @@ namespace Kapitelaufgabe2 {
 
     function getSubpage(): string {
         return window.location.pathname.substring(window.location.pathname.lastIndexOf("/") + 1);
+    }
+
+    interface BrowserCacheData {
+        error: string;
+        message: string;
+    }
+
+    async function getServerAnswer(_url: RequestInfo): Promise<string> {
+        let browserCacheData: JSON = JSON.parse(JSON.stringify(sessionStorage));
+        let query: URLSearchParams = new URLSearchParams(<any>browserCacheData);
+        _url = _url + "?" + query.toString();
+        let response: Response = await fetch(_url);
+        let text: string = await response.text();
+        let displayText: BrowserCacheData = JSON.parse(text);
+        if (displayText.error != undefined) return displayText.error;
+        if (displayText.message != undefined) return displayText.message;
+        return "Ooops, something went wrong.";
     }
 
     //creating basic structure header main and footer in the body
@@ -40,6 +48,11 @@ namespace Kapitelaufgabe2 {
     homeLink.setAttribute("style", "padding: " + 20 + "px; margin: " + 5 + "px; font-size: " + 40 + "px; text-transform: capitalize; font-weight: bold");
     homeLink.setAttribute("href", "../html/index.html");
     header.appendChild(homeLink);
+
+    // creating div for server message on finish.html
+    let showServerMessage: HTMLDivElement = document.createElement("div");
+    showServerMessage.setAttribute("id", "ServerMessage");
+    main.appendChild(showServerMessage);
 
     //creating the subtext of the headline for that specific site without anything in it
     let infoLine: HTMLParagraphElement = document.createElement("p");
@@ -95,9 +108,9 @@ namespace Kapitelaufgabe2 {
             sessionStorage.setItem(_bodyPart + "Picture", _part.picture);
             sessionStorage.setItem(_bodyPart + "Name", _part.name);
             sessionStorage.setItem(_bodyPart + "Animal", _part.animal);
-            if (_bodyPart == "head") window.open("../Kapitelaufgabe_2/body.html", "_self");
-            if (_bodyPart == "body") window.open("../Kapitelaufgabe_2/feet.html", "_self");
-            if (_bodyPart == "feet") window.open("../Kapitelaufgabe_2/finish.html", "_self");
+            if (_bodyPart == "head") window.open("../html/body.html", "_self");
+            if (_bodyPart == "body") window.open("../html/feet.html", "_self");
+            if (_bodyPart == "feet") window.open("../html/finish.html", "_self");
         }
     }
 
@@ -123,82 +136,86 @@ namespace Kapitelaufgabe2 {
 
 
 
+    async function buildPage(): Promise<void> {
+        await getData("../source/data.json");
+        if (currentPage == "index.html") {
 
-    if (currentPage == "index.html") {
+            let info1: HTMLParagraphElement = document.createElement("p");
+            info1.innerText = "Als Projekt für mein Studium möchte ich auf dieser Webseite den Benutzer einen Entenhausener aus Bildern ausgewählter Figuren zusammenbauen lassen.";
+            main.appendChild(info1);
 
-        let info1: HTMLParagraphElement = document.createElement("p");
-        info1.innerText = "Als Projekt für mein Studium möchte ich auf dieser Webseite den Benutzer einen Entenhausener aus Bildern ausgewählter Figuren zusammenbauen lassen.";
-        main.appendChild(info1);
+            let info2: HTMLParagraphElement = document.createElement("p");
+            info2.innerText = "Bitte drücken Sie auf den untenstehenden Button, um zu beginnen.";
+            main.appendChild(info2);
 
-        let info2: HTMLParagraphElement = document.createElement("p");
-        info2.innerText = "Bitte drücken Sie auf den untenstehenden Button, um zu beginnen.";
-        main.appendChild(info2);
+            let startButton: HTMLButtonElement = document.createElement("button");
+            startButton.innerText = "Los geht's!";
+            startButton.addEventListener("click", handleStartButtonClick);
+            main.appendChild(startButton);
 
-        let startButton: HTMLButtonElement = document.createElement("button");
-        startButton.innerText = "Los geht's!";
-        startButton.addEventListener("click", handleStartButtonClick);
-        main.appendChild(startButton);
+            function handleStartButtonClick(_event: MouseEvent): void {
+                window.open("../html/head.html", "_self");
+            }
+        }
 
-        function handleStartButtonClick(_event: MouseEvent): void {
-            window.open("../html/head.html", "_self");
+
+        if (currentPage == "head.html") {
+            for (let i: number = 0; i < parsedData.heads.length; i++) {
+                infoLine.innerText = "Bitte wählen Sie einen Kopf, um Ihren eigenen Entenhausener zusammenzusetzen.";
+                createAPart(parsedData.heads[i], "head");
+            }
+        }
+
+        if (currentPage == "body.html") {
+            showChoices();
+            for (let i: number = 0; i < parsedData.bodies.length; i++) {
+                infoLine.innerText = "Bitte wählen Sie einen Körper, um Ihren eigenen Entenhausener zusammenzusetzen.";
+                createAPart(parsedData.bodies[i], "body");
+            }
+        }
+
+        if (currentPage == "feet.html") {
+            showChoices();
+            for (let i: number = 0; i < parsedData.feet.length; i++) {
+                infoLine.innerText = "Bitte wählen Sie die Füße, um Ihren eigenen Entenhausener zusammenzusetzen.";
+                createAPart(parsedData.feet[i], "feet");
+            }
+        }
+
+        if (currentPage == "finish.html") {
+            
+            showServerMessage.innerText = "Server sagt: " + await getServerAnswer("https://gis-communication.herokuapp.com");
+            
+
+            infoLine.innerText = "Das ist Ihr Entenhausener:";
+            showChoices();
+
+            let name: HTMLParagraphElement = document.createElement("p");
+            let animal: HTMLParagraphElement = document.createElement("p");
+
+            name.innerText = "Name: " + sessionStorage.getItem("headName") + sessionStorage.getItem("bodyName") + sessionStorage.getItem("feetName");
+            animal.innerText = "Tier: " + sessionStorage.getItem("headAnimal") + "-" + sessionStorage.getItem("bodyAnimal") + "-" + sessionStorage.getItem("feetAnimal");
+
+            choicesSoFar.appendChild(name);
+            choicesSoFar.appendChild(animal);
+
+            let thankYouLine: HTMLParagraphElement = document.createElement("p");
+            thankYouLine.innerText = "Danke, dass Sie meine Website benutzt haben. Viel Spaß mit Ihrem eigenen Entenhausener.";
+            thankYouLine.setAttribute("style", "margin-bottom: " + 20 + "px");
+            main.appendChild(thankYouLine);
+
+            let newStart: HTMLButtonElement = document.createElement("button");
+            newStart.innerText = "Versuchen wir das nochmal.";
+            newStart.addEventListener("click", handleButtonClick);
+            main.appendChild(newStart);
+
+            function handleButtonClick(_event: MouseEvent): void {
+                window.open("../html/head.html", "_self");
+            }
+
         }
     }
 
-
-    if (currentPage == "head.html") {
-        for (let i: number = 0; i < parsedData.heads.length; i++) {
-            infoLine.innerText = "Bitte wählen Sie einen Kopf, um Ihren eigenen Entenhausener zusammenzusetzen.";
-            createAPart(parsedData.heads[i], "head");
-        }
-    }
-
-    if (currentPage == "body.html") {
-        showChoices();
-        for (let i: number = 0; i < parsedData.bodies.length; i++) {
-            infoLine.innerText = "Bitte wählen Sie einen Körper, um Ihren eigenen Entenhausener zusammenzusetzen.";
-            createAPart(parsedData.bodies[i], "body");
-        }
-    }
-
-    if (currentPage == "feet.html") {
-        showChoices();
-        for (let i: number = 0; i < parsedData.feet.length; i++) {
-            infoLine.innerText = "Bitte wählen Sie die Füße, um Ihren eigenen Entenhausener zusammenzusetzen.";
-            createAPart(parsedData.feet[i], "feet");
-        }
-    }
-
-    if (currentPage == "finish.html") {
-        infoLine.innerText = "Das ist Ihr Entenhausener:";
-        showChoices();
-
-        let name: HTMLParagraphElement = document.createElement("p");
-        let animal: HTMLParagraphElement = document.createElement("p");
-
-        name.innerText = "Name: " + sessionStorage.getItem("headName") + sessionStorage.getItem("bodyName") + sessionStorage.getItem("feetName");
-        animal.innerText = "Tier: " + sessionStorage.getItem("headAnimal") + "-" + sessionStorage.getItem("bodyAnimal") + "-" + sessionStorage.getItem("feetAnimal");
-
-        choicesSoFar.appendChild(name);
-        choicesSoFar.appendChild(animal);
-
-        let thankYouLine: HTMLParagraphElement = document.createElement("p");
-        thankYouLine.innerText = "Danke, dass Sie meine Website benutzt haben. Viel Spaß mit Ihrem eigenen Entenhausener.";
-        thankYouLine.setAttribute("style", "margin-bottom: " + 20 + "px");
-        main.appendChild(thankYouLine);
-
-        let newStart: HTMLButtonElement = document.createElement("button");
-        newStart.innerText = "Versuchen wir das nochmal.";
-        newStart.addEventListener("click", handleButtonClick);
-        main.appendChild(newStart);
-
-        function handleButtonClick(_event: MouseEvent): void {
-            window.open("../Kapitelaufgabe_2/head.html", "_self");
-        }
-    }
-
-
-
-
-
+    buildPage();
 
 }
