b54afc1d82cade61652358c86760059279e7bde5
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-05-28 14:35:20 +0200

    updated script.ts

--- Kapitel_3/Kapitel_3.1/script/script.js
+++ Kapitel_3/Kapitel_3.1/script/script.js
@@ -1,3 +1,14 @@
 "use strict";
 let formData = new FormData(document.forms[0]);
+let button = document.getElementById("button");
+button.addEventListener("click", handleButtonClick);
+async function handleButtonClick() {
+    let url = "https://gis-server-git-gud.herokuapp.com";
+    //tslint:disable-next-line: no-any
+    let query = new URLSearchParams(formData);
+    url += "/?" + query.toString();
+    let response = await fetch(url);
+    let loggedResponse = await response.text();
+    console.log(loggedResponse);
+}
 //# sourceMappingURL=script.js.map
--- Kapitel_3/Kapitel_3.1/script/script.ts
+++ Kapitel_3/Kapitel_3.1/script/script.ts
@@ -1,1 +1,15 @@
 let formData: FormData = new FormData(document.forms[0]);
+
+let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+button.addEventListener("click", handleButtonClick);
+
+async function handleButtonClick(): Promise<void> {
+    let url: string = "https://gis-server-git-gud.herokuapp.com";
+    //tslint:disable-next-line: no-any
+    let query: URLSearchParams = new URLSearchParams(<any>formData);
+    url += "/?" + query.toString();
+
+    let response: Response = await fetch(url);
+    let loggedResponse: string = await response.text();
+    console.log(loggedResponse);
+}
