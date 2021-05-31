d476b4f4624c2f9bd66c59a530358b193b8d99a3
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-05-31 12:32:47 +0200

    two functions for two buttons

--- Kapitel_3/Kapitel_3.2/script/script.js
+++ Kapitel_3/Kapitel_3.2/script/script.js
@@ -1,20 +1,32 @@
 "use strict";
 var Aufgabe_3_2;
 (function (Aufgabe_3_2) {
-    let button = document.getElementById("button");
-    button.addEventListener("click", handleButtonClick);
-    function handleButtonClick() {
-        getServerAnswer("https://gis-server-git-gud.herokuapp.com");
-        console.log("Button wurde gedrückt.");
-    }
-    async function getServerAnswer(_url) {
+    let htmlButton = document.getElementById("html");
+    htmlButton.addEventListener("click", handleHTMLButtonClick);
+    let jsonButton = document.getElementById("json");
+    jsonButton.addEventListener("click", handleJSONButtonClick);
+    let displayDiv = document.getElementById("htmlAnswer");
+    let url = "http://localhost:8100";
+    async function handleHTMLButtonClick() {
+        console.log("HTML-Button wurde gedrückt.");
         let formData = new FormData(document.forms[0]);
         //tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
-        _url += "?" + query.toString();
-        let response = await fetch(_url);
-        let loggedResponse = await response.text();
-        console.log("query lautet: " + loggedResponse);
+        url += "/html" + "?" + query.toString();
+        let response = await fetch(url);
+        let displayResponse = await response.text();
+        displayDiv.innerText = displayResponse;
+        console.log(displayResponse);
+    }
+    async function handleJSONButtonClick() {
+        console.log("JSON-Button wurde gedrückt.");
+        let formData = new FormData(document.forms[0]);
+        //tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        url += "/json" + "?" + query.toString();
+        let response = await fetch(url);
+        let consoleResponse = await response.json();
+        console.log(consoleResponse);
     }
 })(Aufgabe_3_2 || (Aufgabe_3_2 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel_3/Kapitel_3.2/script/script.ts
+++ Kapitel_3/Kapitel_3.2/script/script.ts
@@ -1,21 +1,34 @@
 namespace Aufgabe_3_2 {
 
-    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-    button.addEventListener("click", handleButtonClick);
+    let htmlButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("html");
+    htmlButton.addEventListener("click", handleHTMLButtonClick);
+    let jsonButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("json");
+    jsonButton.addEventListener("click", handleJSONButtonClick);
+    let displayDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("htmlAnswer");
+    let url: string = "http://localhost:8100";
 
-    function handleButtonClick(): void {
-        getServerAnswer("https://gis-server-git-gud.herokuapp.com");
-        console.log("Button wurde gedrückt.");
-    }
-
-    async function getServerAnswer(_url: RequestInfo): Promise<void> {
+    async function handleHTMLButtonClick(): Promise<void> {
+        console.log("HTML-Button wurde gedrückt.");
         let formData: FormData = new FormData(document.forms[0]);
         //tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        _url += "?" + query.toString();
+        url += "/html" + "?" + query.toString();
 
-        let response: Response = await fetch(_url);
-        let loggedResponse: string = await response.text();
-        console.log("query lautet: " + loggedResponse);
+        let response: Response = await fetch(url);
+        let displayResponse: string = await response.text();
+        displayDiv.innerText = displayResponse;
+        console.log(displayResponse);
+    }
+
+    async function handleJSONButtonClick(): Promise<void> {
+        console.log("JSON-Button wurde gedrückt.");
+        let formData: FormData = new FormData(document.forms[0]);
+        //tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += "/json" + "?" + query.toString();
+
+        let response: Response = await fetch(url);
+        let consoleResponse: string = await response.json();
+        console.log(consoleResponse);
     }
 }
