99c08d5d6e8346be16ab26de95de6e24b5beb88a
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-06-15 17:23:35 +0200

    cloned Kapitel_3.2

---
+++ Kapitel_3/Kapitel_3.4/script/script.js
@@ -0,0 +1,46 @@
+"use strict";
+var Aufgabe_3_4;
+(function (Aufgabe_3_4) {
+    let htmlButton = document.getElementById("html");
+    htmlButton.addEventListener("click", handleHTMLButtonClick);
+    let jsonButton = document.getElementById("json");
+    jsonButton.addEventListener("click", handleJSONButtonClick);
+    let htmlAnswer = document.getElementById("htmlAnswer");
+    let url;
+    let query;
+    function getURL() {
+        //url = "https://gis-server-git-gud.herokuapp.com";
+        url = "http://localhost:8100";
+    }
+    function getFormData() {
+        let formData = new FormData(document.forms[0]);
+        //tslint:disable-next-line: no-any
+        query = new URLSearchParams(formData);
+    }
+    async function handleHTMLButtonClick() {
+        getURL();
+        getFormData();
+        console.log("HTML-Button wurde gedrückt.");
+        url += "/html" + "?" + query.toString();
+        let response = await fetch(url);
+        let displayResponse = await response.text();
+        htmlAnswer.innerText = displayResponse;
+        console.log(displayResponse);
+        // setback of URL to prevent requests with multiple inputs
+        getURL();
+    }
+    async function handleJSONButtonClick() {
+        getURL();
+        getFormData();
+        console.log("JSON-Button wurde gedrückt.");
+        console.log(query);
+        url += "/json" + "?" + query.toString();
+        let response = await fetch(url);
+        let jsonResponse = await response.json();
+        htmlAnswer.innerText = "Bitte sehen Sie in der Konsole nach.";
+        console.log(jsonResponse);
+        // setback of URL to prevent requests with multiple inputs
+        getURL();
+    }
+})(Aufgabe_3_4 || (Aufgabe_3_4 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Kapitel_3/Kapitel_3.4/script/script.ts
@@ -0,0 +1,56 @@
+
+
+namespace Aufgabe_3_4 {
+
+    let htmlButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("html");
+    htmlButton.addEventListener("click", handleHTMLButtonClick);
+    let jsonButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("json");
+    jsonButton.addEventListener("click", handleJSONButtonClick);
+    let htmlAnswer: HTMLDivElement = <HTMLDivElement>document.getElementById("htmlAnswer");
+    let url: string;
+    let query: URLSearchParams;
+
+    function getURL(): void {
+        //url = "https://gis-server-git-gud.herokuapp.com";
+        url = "http://localhost:8100";
+    }
+
+    function getFormData(): void {
+        let formData: FormData = new FormData(document.forms[0]);
+        //tslint:disable-next-line: no-any
+        query = new URLSearchParams(<any>formData);
+    }
+
+    async function handleHTMLButtonClick(): Promise<void> {
+        getURL();
+        getFormData();
+
+        console.log("HTML-Button wurde gedrückt.");
+
+        url += "/html" + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let displayResponse: string = await response.text();
+        htmlAnswer.innerText = displayResponse;
+        console.log(displayResponse);
+        
+        // setback of URL to prevent requests with multiple inputs
+        getURL();
+    }
+
+    async function handleJSONButtonClick(): Promise<void> {
+        getURL();
+        getFormData();
+
+        console.log("JSON-Button wurde gedrückt.");
+        console.log(query);
+        
+        url += "/json" + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let jsonResponse: string = await response.json();
+        htmlAnswer.innerText = "Bitte sehen Sie in der Konsole nach.";
+        console.log(jsonResponse);
+
+        // setback of URL to prevent requests with multiple inputs
+        getURL();
+    }
+}
