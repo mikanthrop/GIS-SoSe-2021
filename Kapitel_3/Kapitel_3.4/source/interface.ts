112834a13d117fcc23c42a508bc2d8f4669af910
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-06-19 14:24:06 +0200

    getting around namespaces
    -

--- Kapitel_3/Kapitel_3.4/source/interface.ts
+++ Kapitel_3/Kapitel_3.4/source/interface.ts
@@ -1,11 +1,9 @@
 import { ObjectID } from "mongodb";
 
-export namespace Kapiteldreivier {
-    export interface Rant {
-        _id: ObjectID;
-        user: string;
-        category: string;
-        title: string;
-        rant: string;
-    }
+export interface Rant {
+    _id: ObjectID;
+    user: string;
+    category: string;
+    title: string;
+    rant: string;
 }
