b847ed1f87c9cc443e897a2fca08b64e808ee1de
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-06-28 17:48:43 +0200

    reorganized script
    comments are legacy code, will edit out, when it's safe to assume that i don't need it there anymore

--- Endabgabe/scripts/login.js
+++ Endabgabe/scripts/login.js
@@ -1,17 +1,36 @@
 "use strict";
 var Endabgabe;
 (function (Endabgabe) {
+    //generally used elements/variables
+    let formData;
+    let serverResponseDiv = document.getElementById("serverResponse");
+    let query;
+    let url;
+    //login.html
     document.getElementById("switchToSignup").addEventListener("click", handleClickSwitchToSignup);
     document.getElementById("switchToLogin").addEventListener("click", handleClickSwitchToLogin);
     document.getElementById("buttonLogin").addEventListener("click", handleClickButtonLogin);
     document.getElementById("buttonSignup").addEventListener("click", handleClickButtonSignup);
     let loginForm = document.getElementById("loginForm");
     let signupForm = document.getElementById("signupForm");
-    let formData;
-    let serverResponseDiv = document.getElementById("serverResponse");
-    let url;
-    let query;
-    //make switching between login and signup form possible
+    /* //myRecipes.html
+     document.getElementById("submitRecipe").addEventListener("click", handleClickSubmitRecipe);
+     document.getElementById("addIngredient").addEventListener("click", handleClickAddIngredient);
+     let ingredientsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("ingredients");
+     let recipeForm: HTMLFormElement = <HTMLFormElement>document.getElementById("recipeForm");
+     let ingredientList: string[];
+     let ingredientCount: number = 1;*/
+    // generally used functions
+    function getURL() {
+        //url = "https://gis-server-git-gud.herokuapp.com";
+        url = "http://localhost:8100";
+    }
+    function setQuery(_formData) {
+        //tslint:disable-next-line: no-any
+        query = new URLSearchParams(_formData);
+    }
+    //login.html functions
+    //makes switching between login and signup form possible
     function handleClickSwitchToLogin() {
         console.log("SwitchToLogin wurde gedrückt.");
         signupForm.classList.add("ishidden");
@@ -21,14 +40,6 @@ var Endabgabe;
         console.log("SwitchToSignup wurde gedrückt.");
         loginForm.classList.add("ishidden");
         signupForm.classList.remove("ishidden");
-    }
-    function getURL() {
-        //url = "https://gis-server-git-gud.herokuapp.com";
-        url = "http://localhost:8100";
-    }
-    function setQuery(_formData) {
-        //tslint:disable-next-line: no-any
-        query = new URLSearchParams(_formData);
     }
     function getFormDataLogin() {
         formData = new FormData(loginForm);
@@ -53,6 +64,7 @@ var Endabgabe;
             sessionStorage.setItem("user", formData.get("user").toString());
             //recipefavs?
             //sessionStorage.setItem("")
+            window.open("../html/recipes.html", "_self");
         }
     }
     async function handleClickButtonSignup() {
@@ -68,5 +80,26 @@ var Endabgabe;
         else
             serverResponseDiv.innerHTML = displayResponse;
     }
+    //myRecipes.html functions
+    /* function handleClickAddIngredient(): void {
+         let nextIngredient: HTMLInputElement = document.createElement("input");
+         nextIngredient.type = "text";
+         nextIngredient.name = "ingredient" + ingredientCount;
+         ingredientCount++;
+         ingredientsDiv.appendChild(nextIngredient);
+     }
+ 
+     async function handleClickSubmitRecipe(): Promise<void> {
+         console.log("Submit Recipe wurde gedrückt.");
+         
+         formData = new FormData(recipeForm);
+         console.log(formData.getAll("ingredient"));
+         setQuery(formData);
+         url += "/submit?" + query.toString();
+         let response: Response = await fetch(url);
+         let displayResponse: string = await response.text();
+         serverResponseDiv.innerHTML = displayResponse;
+ 
+     }*/
 })(Endabgabe || (Endabgabe = {}));
 //# sourceMappingURL=login.js.map
--- Endabgabe/scripts/login.ts
+++ Endabgabe/scripts/login.ts
@@ -1,30 +1,31 @@
+
+
 namespace Endabgabe {
+    //generally used elements/variables
+    let formData: FormData;
+    let serverResponseDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("serverResponse");
+    let query: URLSearchParams;
+
+    let url: string;
+
+    //login.html
     document.getElementById("switchToSignup").addEventListener("click", handleClickSwitchToSignup);
     document.getElementById("switchToLogin").addEventListener("click", handleClickSwitchToLogin);
     document.getElementById("buttonLogin").addEventListener("click", handleClickButtonLogin);
     document.getElementById("buttonSignup").addEventListener("click", handleClickButtonSignup);
     let loginForm: HTMLFormElement = <HTMLFormElement>document.getElementById("loginForm");
     let signupForm: HTMLFormElement = <HTMLFormElement>document.getElementById("signupForm");
-    let formData: FormData;
-    let serverResponseDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("serverResponse");
-    let url: string;
-    let query: URLSearchParams;
 
-    //make switching between login and signup form possible
-    function handleClickSwitchToLogin(): void {
-        console.log("SwitchToLogin wurde gedrückt.");
-        
-        signupForm.classList.add("ishidden");
-        loginForm.classList.remove("ishidden");
-    }
+   /* //myRecipes.html
+    document.getElementById("submitRecipe").addEventListener("click", handleClickSubmitRecipe);
+    document.getElementById("addIngredient").addEventListener("click", handleClickAddIngredient);
+    let ingredientsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("ingredients");
+    let recipeForm: HTMLFormElement = <HTMLFormElement>document.getElementById("recipeForm");
+    let ingredientList: string[];
+    let ingredientCount: number = 1;*/
 
-    function handleClickSwitchToSignup(): void {
-        console.log("SwitchToSignup wurde gedrückt.");
-        
-        loginForm.classList.add("ishidden");
-        signupForm.classList.remove("ishidden");
-    }
 
+    // generally used functions
     function getURL(): void {
         //url = "https://gis-server-git-gud.herokuapp.com";
         url = "http://localhost:8100";
@@ -33,6 +34,23 @@ namespace Endabgabe {
     function setQuery(_formData: FormData): void {
         //tslint:disable-next-line: no-any
         query = new URLSearchParams(<any>_formData);
+    }
+
+
+    //login.html functions
+    //makes switching between login and signup form possible
+    function handleClickSwitchToLogin(): void {
+        console.log("SwitchToLogin wurde gedrückt.");
+
+        signupForm.classList.add("ishidden");
+        loginForm.classList.remove("ishidden");
+    }
+
+    function handleClickSwitchToSignup(): void {
+        console.log("SwitchToSignup wurde gedrückt.");
+
+        loginForm.classList.add("ishidden");
+        signupForm.classList.remove("ishidden");
     }
 
     function getFormDataLogin(): void {
@@ -49,7 +67,6 @@ namespace Endabgabe {
 
     async function handleClickButtonLogin(): Promise<void> {
         console.log("ButtonLogin wurde gedrückt. Server sieht nach, ob Eingabedaten mit Datenbank übereinstimmen.");
-        
         getURL();
         getFormDataLogin();
 
@@ -62,22 +79,46 @@ namespace Endabgabe {
             sessionStorage.setItem("user", formData.get("user").toString());
             //recipefavs?
             //sessionStorage.setItem("")
+            window.open("../html/recipes.html", "_self");
         }
     }
 
     async function handleClickButtonSignup(): Promise<void> {
         console.log("ButtonSignup wurde gedrückt. Server erstellt ein neues Nutzerprofil.");
-        
         getURL();
         getFormDataSignup();
 
-        url += "/signup?" +  query.toString();
+        url += "/signup?" + query.toString();
         let response: Response = await fetch(url);
         let displayResponse: string = await response.text();
         console.log(displayResponse);
         if (displayResponse == "Ihr Account wurde erstellt.") window.open("../html/login.html", "_self");
-        else 
+        else
+            serverResponseDiv.innerHTML = displayResponse;
+    }
+
+
+    //myRecipes.html functions
+
+
+   /* function handleClickAddIngredient(): void {
+        let nextIngredient: HTMLInputElement = document.createElement("input");
+        nextIngredient.type = "text";
+        nextIngredient.name = "ingredient" + ingredientCount;
+        ingredientCount++;
+        ingredientsDiv.appendChild(nextIngredient);
+    }
+
+    async function handleClickSubmitRecipe(): Promise<void> {
+        console.log("Submit Recipe wurde gedrückt.");
+        
+        formData = new FormData(recipeForm);
+        console.log(formData.getAll("ingredient"));
+        setQuery(formData);
+        url += "/submit?" + query.toString();
+        let response: Response = await fetch(url);
+        let displayResponse: string = await response.text();
         serverResponseDiv.innerHTML = displayResponse;
 
-    }
+    }*/
 }
