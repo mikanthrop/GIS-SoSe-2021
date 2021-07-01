d85a14eeb0aaae106800ee64993d823fccfc3fcc
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2021-07-01 20:37:38 +0200

    added functionality to myRecipe.ts
    - changed user to localStorage instead of sessionStorage
    - cannot send form without all blancs filled in
    - started writing fetch request to server and how to deal with answer

--- Endabgabe/scripts/myRecipes.js
+++ Endabgabe/scripts/myRecipes.js
@@ -1,17 +1,29 @@
 "use strict";
 var Endabgabe;
 (function (Endabgabe) {
+    let ingredientsDiv = document.getElementById("ingredients");
+    let loggedInDiv = document.getElementById("loggedIn");
+    let recipeForm = document.getElementById("recipeForm");
     let serverResponseDiv = document.getElementById("serverResponse");
+    let scriptResponseDiv = document.getElementById("scriptResponse");
     let query;
     let ingredientInput;
     let url;
-    document.getElementById("submitRecipe").addEventListener("click", handleClickSubmitRecipe);
-    document.getElementById("addIngredient").addEventListener("click", handleClickAddIngredient);
-    let ingredientsDiv = document.getElementById("ingredients");
-    let recipeForm = document.getElementById("recipeForm");
     let recipeData;
     let ingredientList;
     let ingredientCount = 0;
+    window.addEventListener("load", handleLoggedIn);
+    document.getElementById("submitRecipe").addEventListener("click", handleClickSubmitRecipe);
+    document.getElementById("addIngredient").addEventListener("click", handleClickAddIngredient);
+    function handleLoggedIn() {
+        console.log(localStorage.getItem("user"));
+        if (localStorage.getItem("user") == null) {
+            loggedInDiv.classList.add("ishidden");
+            let notYou = document.createElement("h2");
+            notYou.innerHTML = "Sie müssen angemeldet sein, um dieses Feature nutzen zu können.";
+            serverResponseDiv.appendChild(notYou);
+        }
+    }
     function getURL() {
         //url = "https://gis-server-git-gud.herokuapp.com";
         url = "http://localhost:8100";
@@ -33,27 +45,45 @@ var Endabgabe;
             serverResponseDiv.innerHTML = "Bitte geben Sie eine weitere Zutat ein.";
     }
     async function handleClickSubmitRecipe() {
+        scriptResponseDiv.innerHTML = "";
         console.log("Submit Recipe wurde gedrückt.");
         getURL();
-        let recipe;
         console.log(ingredientsDiv.childElementCount);
+        let recipeTitle = document.getElementById("title");
         ingredientInput = document.getElementById("ingredient0");
-        ingredientList = [ingredientInput.value];
-        //appends every ingredient to ingredientList
-        for (let i = 1; i < ingredientsDiv.childElementCount; i++) {
-            console.log("----------i'm in------------");
-            ingredientInput = document.getElementById("ingredient" + i);
-            console.log(ingredientInput.value);
-            ingredientList.push(ingredientInput.value);
-            console.log(ingredientList);
+        let recipePreparation = document.getElementById("preparationTextfield");
+        if (!recipeTitle.value)
+            scriptResponseDiv.innerHTML = "Bitte geben Sie einen Titel ein.";
+        else if (!ingredientInput.value)
+            scriptResponseDiv.innerHTML = "Bitte geben Sie eine Zutat ein.";
+        else if (!recipePreparation.value)
+            scriptResponseDiv.innerHTML = "Bitte geben Sie an, wie das Gericht zubereitet wird.";
+        else {
+            ingredientList = [ingredientInput.value];
+            //appends every ingredient to ingredientList
+            for (let i = 1; i < ingredientsDiv.childElementCount; i++) {
+                console.log("----------i'm in------------");
+                console.log("child Elements " + ingredientsDiv.childElementCount);
+                ingredientInput = document.getElementById("ingredient" + i);
+                console.log("value " + ingredientInput.value);
+                console.log(!ingredientInput.value);
+                if (!ingredientInput.value == false) {
+                    ingredientList.push(ingredientInput.value);
+                }
+                else
+                    console.log("--------------i'm out--------------");
+                console.log("Zutatenliste " + ingredientList);
+            }
+            let recipe = { ingredients: ingredientList, author: localStorage.getItem("user"), title: recipeTitle.value, preparation: recipePreparation.value };
+            console.log("recipe ", recipe);
+            // gotta make the query by myself
+            url += "/submit?title=" + recipe.title + "&author=" + recipe.author + "&ingredients=" + recipe.ingredients + "&preparation=" + recipe.preparation;
+            console.log(url);
+            let response = await fetch(url);
+            let displayResponse = await response.text();
+            serverResponseDiv.innerHTML = displayResponse;
+            recipeForm.reset();
         }
-        recipe.ingredients = ingredientList;
-        console.log(recipeData.getAll("ingredient"));
-        // gotta make the query by myself
-        url += "/submit?" + query.toString();
-        let response = await fetch(url);
-        let displayResponse = await response.text();
-        serverResponseDiv.innerHTML = displayResponse;
     }
 })(Endabgabe || (Endabgabe = {}));
 //# sourceMappingURL=myRecipes.js.map
--- Endabgabe/scripts/myRecipes.ts
+++ Endabgabe/scripts/myRecipes.ts
@@ -1,16 +1,31 @@
 namespace Endabgabe {
+
+    let ingredientsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("ingredients");
+    let loggedInDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("loggedIn");
+    let recipeForm: HTMLFormElement = <HTMLFormElement>document.getElementById("recipeForm");
     let serverResponseDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("serverResponse");
+    let scriptResponseDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("scriptResponse");
+
     let query: URLSearchParams;
     let ingredientInput: HTMLInputElement;
     let url: string;
-    
-    document.getElementById("submitRecipe").addEventListener("click", handleClickSubmitRecipe);
-    document.getElementById("addIngredient").addEventListener("click", handleClickAddIngredient);
-    let ingredientsDiv: HTMLDivElement = <HTMLDivElement>document.getElementById("ingredients");
-    //let recipeForm: HTMLFormElement = <HTMLFormElement>document.getElementById("recipeForm");
     let recipeData: FormData;
     let ingredientList: string[];
     let ingredientCount: number = 0;
+
+    window.addEventListener("load", handleLoggedIn);
+    document.getElementById("submitRecipe").addEventListener("click", handleClickSubmitRecipe);
+    document.getElementById("addIngredient").addEventListener("click", handleClickAddIngredient);
+
+    function handleLoggedIn(): void {
+        console.log(localStorage.getItem("user"));
+        if (localStorage.getItem("user") == null) {
+            loggedInDiv.classList.add("ishidden");
+            let notYou: HTMLElement = document.createElement("h2");
+            notYou.innerHTML = "Sie müssen angemeldet sein, um dieses Feature nutzen zu können.";
+            serverResponseDiv.appendChild(notYou);
+        }
+    }
 
     function getURL(): void {
         //url = "https://gis-server-git-gud.herokuapp.com";
@@ -19,10 +34,10 @@ namespace Endabgabe {
 
     function handleClickAddIngredient(): void {
         serverResponseDiv.innerHTML = "";
-        
+
         let thisIngredient: string = "ingredient" + ingredientCount;
         ingredientInput = <HTMLInputElement>document.getElementById(thisIngredient);
-       
+
         if (ingredientInput.value != "") {
             ingredientCount++;
             thisIngredient = "ingredient" + ingredientCount;
@@ -37,28 +52,43 @@ namespace Endabgabe {
     }
 
     async function handleClickSubmitRecipe(): Promise<void> {
+        scriptResponseDiv.innerHTML = "";
         console.log("Submit Recipe wurde gedrückt.");
         getURL();
 
-        let recipe: Recipe;
         console.log(ingredientsDiv.childElementCount);
+        let recipeTitle: HTMLInputElement = <HTMLInputElement>document.getElementById("title");
         ingredientInput = <HTMLInputElement>document.getElementById("ingredient0");
-        ingredientList = [ingredientInput.value];
+        let recipePreparation: HTMLInputElement = <HTMLInputElement>document.getElementById("preparationTextfield");
 
-        //appends every ingredient to ingredientList
-        for (let i: number = 1; i < ingredientsDiv.childElementCount; i++) {
-            console.log("----------i'm in------------");
-            ingredientInput = <HTMLInputElement>document.getElementById("ingredient" + i);
-            console.log(ingredientInput.value);
-            ingredientList.push(ingredientInput.value);
-            console.log(ingredientList);
+        if (!recipeTitle.value) scriptResponseDiv.innerHTML = "Bitte geben Sie einen Titel ein.";
+        else if (!ingredientInput.value) scriptResponseDiv.innerHTML = "Bitte geben Sie eine Zutat ein.";
+        else if (!recipePreparation.value) scriptResponseDiv.innerHTML = "Bitte geben Sie an, wie das Gericht zubereitet wird.";
+        else {
+            ingredientList = [ingredientInput.value];
+            //appends every ingredient to ingredientList
+            for (let i: number = 1; i < ingredientsDiv.childElementCount; i++) {
+                console.log("----------i'm in------------");
+                console.log("child Elements " + ingredientsDiv.childElementCount);
+                ingredientInput = <HTMLInputElement>document.getElementById("ingredient" + i);
+                console.log("value " + ingredientInput.value);
+                console.log(!ingredientInput.value);
+                if (!ingredientInput.value == false) {
+                    ingredientList.push(ingredientInput.value);
+                } else console.log("--------------i'm out--------------");
+
+                console.log("Zutatenliste " + ingredientList);
+            }
+            let recipe: Recipe = { ingredients: ingredientList, author: localStorage.getItem("user"), title: recipeTitle.value, preparation: recipePreparation.value };
+            console.log("recipe ", recipe);
+            // gotta make the query by myself
+            url += "/submit?title=" + recipe.title + "&author=" + recipe.author + "&ingredients=" + recipe.ingredients + "&preparation=" + recipe.preparation;
+            console.log(url);
+            let response: Response = await fetch(url);
+            let displayResponse: string = await response.text();
+            serverResponseDiv.innerHTML = displayResponse;
+            
+            recipeForm.reset();
         }
-        recipe.ingredients = ingredientList;
-        console.log(recipeData.getAll("ingredient"));
-        // gotta make the query by myself
-        url += "/submit?" + query.toString();
-        let response: Response = await fetch(url);
-        let displayResponse: string = await response.text();
-        serverResponseDiv.innerHTML = displayResponse;
     }
 }
